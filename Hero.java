4f3fe30e91ef0832d92e8d1a553a2199d5f9dbf3
snjezana.prgic@telekom.de <snjezana.prgic@telekom.de>
2023-08-28 17:19:53 +0200

    Thanos

---
+++ Hero.java
@@ -0,0 +1,35 @@
+
+
+
+
+    public class Hero implements Comparable<Hero> {
+
+
+        private String name;
+        private int age;
+
+
+        public Hero(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int compareTo(Hero other) {
+            return this.name.compareTo(other.name);
+        }
+    }
+
+
---
+++ Thanos.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+    public class  Thanos {
+
+        public static void main(String[] args) {
+
+            List<Hero> heroes = new ArrayList<>();
+            heroes.add(new Hero("Hulk", 49));
+            heroes.add(new Hero("Black Widow", 34));
+            heroes.add(new Hero("Captain America", 100));
+            heroes.add(new Hero("Thor", 1501));
+
+            Collections.sort(heroes);
+
+            System.out.println("\nOrder by name:");
+            showList(heroes);
+
+            Collections.sort(heroes, new Comparator<Hero>() {
+                @Override
+                public int compare(Hero o1, Hero o2) {
+                    return Integer.compare(o2.getAge(), o1.getAge());
+                }
+            });
+
+            System.out.println("\nOrder by age:");
+            showList(heroes);
+        }
+
+        private static void showList(List<Hero> heroes) {
+            for (Hero hero : heroes) {
+                System.out.println(hero.getName() + ", " + hero.getAge());
+            }
+        }
+    }
+
